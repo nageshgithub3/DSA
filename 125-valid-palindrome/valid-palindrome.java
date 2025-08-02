a2439828c720a88aee6ce0bb36b6e6955f7e30a2
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2025-08-02 23:55:38 +0530

    Time: 13 ms (42.73%) | Memory: 45.1 MB (29.85%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        s = s.replaceAll("[^a-z0-9]","");
+
+        int l = 0;
+        int r = s.length()-1;
+        int n = s.length();
+
+        for(int i=0;i<n/2;i++){
+            if(s.charAt(i) != s.charAt(n-1-i)){
+                return false;
+            }
+           
+        }
+        return true;
+
+        
+        
+    }
+}
